b41a222ae746083c95fa054f15c34c952451e0b0
navarro sergio <sirdio24@hotmail.com>
2022-11-18 19:26:04 -0300

    crando tes unitario

--- geometria/geometria_test.go
+++ geometria/geometria_test.go
@@ -1,3 +1,14 @@
 package geometria
 
+import  (
+	"testing"
 
+"github.com/stretchr/testify/assert"
+
+)
+func TestArea(t *testing.T) {
+
+	got := Area(12.0, 6)
+	want := 36.0
+	assert.Equal(t, got, want)
+}
