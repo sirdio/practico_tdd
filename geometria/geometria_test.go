7356433d607e183148e34ae4d099d73e385afa50
navarro sergio <sirdio24@hotmail.com>
2022-11-18 20:14:06 -0300

    cambios

--- geometria/geometria_test.go
+++ geometria/geometria_test.go
@@ -1,16 +1,15 @@
 package geometria
 
-import  (
+import ( 
 	"testing"
-
-"github.com/stretchr/testify/assert"
-
+ 	"github.com/stretchr/testify/assert"
 )
-
 func TestArea(t *testing.T) {
 
 	got := Area(12.0, 6)
 	want := 36.0
 	assert.Equal(t, got, want)
+
+
 }
 
