eb81bcf1c2ed1b8897cea378c87b386932d5a37f
navarro sergio <sirdio24@hotmail.com>
2022-11-19 11:54:40 -0300

    agregando estructura triangulo

--- geometria/geometria.go
+++ geometria/geometria.go
@@ -1,12 +1,26 @@
 package geometria
 
+/*
+Dados los numeros de base y altura, cuando se llama a la funcion
+CalcularArea, entonces retorna el Area de un Triangulo
+*/
 func Area(base, altura float64) interface{} {
 
 	return (base * altura) / 2
 
 }
 
-
+/*
+Dados tres lados de un triangulo, cuando se llama a la funcion
+CalcularPerimetro, entonces retorna el Perimetro de un Triangulo
+*/
 func Perimetro( lado1, lado2, lado3 float64) interface{} {
 	return lado1 + lado2 + lado3
+}
+
+// estructrua de tipo triangulo que contiene la longitudes de los lados del triangulo
+type Triangulo struct{
+	Lado_1 float64
+	Lado_2 float64
+	Lado_3 float64
 }
